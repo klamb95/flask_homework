65270f1573665e20e20b804c69482917df38d562
klamb95 <klamb1995@gmail.com>
2021-04-14 16:23:17 +0100

    working calc

--- modules/calculator.py
+++ modules/calculator.py
@@ -3,3 +3,9 @@ def add_two_numbers(num_1, num_2):
 
 def subtract_two_numbers(num_1, num_2):
     return num_1 - num_2
+
+def multiply_two_numbers(num_1, num_2):
+    return num_1 * num_2
+
+def divide_two_numbers(num_1, num_2):
+    return num_1 / num_2
--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -9,3 +9,9 @@ class TestCalculator(unittest.TestCase):
 
     def test_calculator_can_subtract(self):
         self.assertEqual(1,subtract_two_numbers(2,1))
+
+    def test_calculator_can_multiply(self):
+        self.assertEqual(10, multiply_two_numbers(2,5))
+
+    def test_calculator_can_divide(self):
+        self.assertEqual(5, divide_two_numbers(10,2))

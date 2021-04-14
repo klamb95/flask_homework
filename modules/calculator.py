d149019b334ca39bf8cb170fa381c27831afc20a
klamb95 <klamb1995@gmail.com>
2021-04-14 16:20:45 +0100

    working subtract

--- modules/calculator.py
+++ modules/calculator.py
@@ -1,2 +1,5 @@
 def add_two_numbers(num_1, num_2):
     return num_1 + num_2
+
+def subtract_two_numbers(num_1, num_2):
+    return num_1 - num_2
--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -6,3 +6,6 @@ class TestCalculator(unittest.TestCase):
 
     def test_calculator_can_add(self):
         self.assertEqual(2,add_two_numbers(1,1))
+
+    def test_calculator_can_subtract(self):
+        self.assertEqual(1,subtract_two_numbers(2,1))
